77dfc93759cb234c8846c872783c7088143c8a51
williamtran955 <william.g.tran@sjsu.edu>
2022-11-30 19:20:38 -0800

    changed date time to correct table

--- journal/text_entry/models.py
+++ journal/text_entry/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
 # The journal column to track individual entries
-# title refrences the entry and has a publishing date associated with it
+# title refrences the entry
 class Journal(models.Model):
 	title_text = models.CharField(max_length=200)
-	pub_date = models.DateTimeField(auto_now_add=True)
 
 # individual entries to track 
 # title links it to the title in journal column
 # has a column for text
 # has a column for an image
+# has a column for the datetime
 class Entry(models.Model):
 	title = models.ForeignKey(Journal, on_delete=models.CASCADE)
 	entry_text = models.TextField()
 	image = models.ImageField(upload_to='./uploads/')
+	pub_date = models.DateTimeField(auto_now_add=True)
