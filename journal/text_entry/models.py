8c3f1c8d3d2215324e0c6692cdaa2051d86eda2e
williamtran955 <william.g.tran@sjsu.edu>
2022-11-30 18:14:53 -0800

    added comments

--- journal/text_entry/models.py
+++ journal/text_entry/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
+# The journal column to track individual entries
+# title refrences the entry and has a publishing date associated with it
 class Journal(models.Model):
 	title_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
+# individual entries to track 
+# title links it to the title in journal column
+# has a column for text
+# has a column for an image
 class Entry(models.Model):
 	title = models.ForeignKey(Journal, on_delete=models.CASCADE)
 	entry_text = models.TextField()
 	image = models.ImageField(upload_to='/uploads/')
-
-# Create your models here.
